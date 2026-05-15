be80f4567097bde9428461c756cdb47905dc8c47
CodeKheb <kherbinbuenaventura@gmail.com>
2026-05-15 20:13:58 +0800

    First Larp

---
+++ C/first/first_program.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/*  
+ *  standard input output header file
+ *  includes printf and scanf
+ *  (.h) header = library
+ *  :Man <syntax>
+ */
+
+// Prototype = promise to the compiler that this function exists
+void count(int n);
+
+int main(void) {
+    int until;
+
+    printf("Until when to count? ");
+
+    // & points to address of variable
+    scanf("%i", &until);
+    count(until);
+}
+
+void count(int n) {
+    for (int i = 0; i <= n; i++){
+        if (i != 0) printf("%i\n", i);
+    /* %i = int
+     * %d = double
+     * %f = float
+     * %s = string
+     * %c = char
+     */
+    }
+}
+
