1a4d57544e1e467d95ceb4a52e52c4f1c0907f86
CodeKheb <kherbinbuenaventura@gmail.com>
2026-05-15 21:21:59 +0800

    calc means calculator btw

---
+++ C/calc/calc.c
@@ -0,0 +1,69 @@
+// Header
+#include <stdio.h>
+
+// Prototypes
+int addition(int first_input, int second_input);
+int subtraction(int first_input, int second_input);
+int multiplication(int first_input, int second_input);
+int division(int first_input, int second_input);
+char calculate(char operator, int first_input, int second_input);
+
+// Main
+int main(void){
+    int first_input;
+    int second_input;
+    char operator;
+
+    printf("First Integer: ");
+    scanf("%i", &first_input);
+
+    printf("Operator (+ - / *): ");
+    // need space here before %c to skip whitespaces in new line 
+    scanf(" %c", &operator);
+
+    printf("Second Integer: ");
+    scanf("%i", &second_input);
+
+    int result = calculate(operator, first_input, second_input);
+    printf("%i", result);
+}
+
+char calculate(char operator, int first_input, int second_input) {
+    int result;
+    if (operator == '+') {
+        result = addition(first_input, second_input);
+    }
+    if (operator == '-') {
+        result = subtraction(first_input, second_input);
+    }
+    if (operator == '*') {
+        result = multiplication(first_input, second_input);
+    }
+    if (operator == '/') {
+        result = division(first_input, second_input);
+    } 
+
+    return result;
+}
+
+int addition(int first_input, int second_input) {
+    int sum = first_input + second_input;
+    return sum;
+}
+
+int subtraction(int first_input, int second_input) {
+    int difference = first_input - second_input;
+    return difference;
+}
+
+int multiplication(int first_input, int second_input) {
+    int product = first_input * second_input;
+    return product;
+}
+
+int division(int first_input, int second_input) {
+    int quotient = first_input / second_input;
+    return quotient;
+}
+
+
