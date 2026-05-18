1821ffe2f6baa36764681241e2c2f68c30368b14
CodeKheb <kherbinbuenaventura@gmail.com>
2026-05-19 02:08:57 +0800

    string.h larping

--- C/algorithms/search.c
+++ C/algorithms/search.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void) {
     // * pointers, for a string array, [0] points to the first character of the array which is "battleship"
@@ -9,11 +10,23 @@ int main(void) {
     // fgets, just like scanf but more for strings
     // takes variable s, the size [100], and standard library input
     fgets(s, 100, stdin);
+    // fgets is weird because it adds a new line automatically at the end 
+    // because of this you can't really strcmp the input properly without removing it
+
+    // remove the new line by making it a NUL terminator
+    int s_length = 0;
+    while (s[s_length] != '\0') {
+        s_length++;
+        if (s[s_length] == '\0') {
+            (s[s_length - 1] = '\0');
+        }
+    }
 
     for (int i = 0; i < 6; i++) {
-        // if input s[i] == pointer to string[i]
-        if (s[i] == *strings[i]) {
-            printf("FOUND");
+        // strcmp from string.h takes two strings as an argument then compares
+        // if it equals to 0, it is the same, otherwise it is not
+        if (strcmp(s, strings[i]) == 0) {
+            printf("FOUND %s", s);
             return 0;
         }
     }
