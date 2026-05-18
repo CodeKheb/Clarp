299ba6665e11f7b06851f5b8edda5b76557327ac
CodeKheb <kherbinbuenaventura@gmail.com>
2026-05-19 03:17:32 +0800

    structs looking a lot like object in oop idk

--- C/data_structures/struct.c
+++ C/data_structures/struct.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 // define new data structure
 typedef struct {
@@ -7,13 +8,40 @@ typedef struct {
 } person;
 
 int main(void) {
-    // initialize 3 people
-    person people[3];
+    // initialize amount of people
+    person people[4];
 
     // dot operator to access variables
     people[0].name = "Kherbin";
     people[0].number= "+63-0933";
 
-    printf("%s", people[0].name);
-    printf("%s", people[0].number);
+    people[1].name = "Josh";
+    people[1].number= "+63-0922";
+    
+    people[2].name = "Wren";
+    people[2].number= "+63-0933";
+
+    people[3].name = "Renzo";
+    people[3].number= "+63-0944";
+
+    char name[100];
+    fgets(name, 100, stdin);
+
+
+    int length = 0;
+    while (name[length] != '\0') {
+        length++;
+        if (name[length] == '\0') {
+            name[length - 1] = '\0';
+        }
+    }
+
+    for (int i = 0; i < 4; i++) {
+        if (strcmp(people[i].name, name) == 0) {
+            printf("Name: %s\n", people[i].name);
+            printf("Number: %s\n", people[i].number);
+            return 0;
+        }
+    }
+    printf("Low tier %s, idk you", name);
 }
