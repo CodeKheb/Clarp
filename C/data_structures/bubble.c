78951637dd2c9d98461bf2c5f88dc77cc3f303f3
CodeKheb <kherbinbuenaventura@gmail.com>
2026-06-26 15:15:10 +0800

    larp larp larp larp

--- C/data_structures/bubble.c
+++ C/data_structures/bubble.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
-int * bubble(int nums[]);
+void * bubble(int nums[], int length);
+int * bubble_print(int nums[], int length);
 
 
 int main(void) {
     int sort[8] = {2, 3, 5, 1, 0, 4, 7, 6};
+    int size = sizeof(sort) / sizeof(int);
 
 
     printf("Array: ");
@@ -12,7 +14,9 @@ int main(void) {
         printf("%i ", sort[i]);
     }
 
-    bubble(sort);
+    int *nums = bubble_print(sort, size);
+
+    bubble(sort, size);
 
     printf("\nSorted?: ");
     for (int i = 0; i < 8; i++) {
@@ -20,11 +24,26 @@ int main(void) {
     }
 }
 
-int * bubble(int nums[]) {
+void * bubble(int nums[], int length) {
     int temp;
 
-    for (int i = 0; i < 8; i++){
-        for (int j = 0; j < 8 - i - 1; j++) {
+    for (int i = 0; i < length; i++) {
+        for (int j = 0; j < length - i - 1; j++) {
+
+            if (nums[j] > nums [j + 1]) {
+                temp = nums[j + 1];
+                nums[j + 1] = nums[j];
+                nums[j] = temp;
+            }
+        }
+    }
+}
+
+int * bubble_print(int nums[], int length) {
+    int temp;
+
+    for (int i = 0; i < length; i++){
+        for (int j = 0; j < length - i - 1; j++) {
 
             if (nums[j] > nums[j + 1]) {
                  temp = nums[j + 1];
@@ -35,3 +54,5 @@ int * bubble(int nums[]) {
     }
     return nums;
 }
+
+    
